08ad58e0b8c68ff679b3f9ac21d17c248f2be3a5
Zhangir009 <z_meiram@kbtu.kz>
2026-02-05 22:55:54 +0500

    Basis

---
+++ practice 1/strings.py
@@ -0,0 +1,22 @@
+# example 1
+a = "Hello, World!"
+print(a[1:6])
+
+# example 2
+a = "Hello, World!"
+print(a.upper())
+
+
+# example 3
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+# example 4
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+# example 5
+txt = "We are the so-called \"Vikings\" from the north."
