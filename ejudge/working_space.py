69207f54acd07a46de0531ac26dca65529c8f7ba
Zhangir009 <z_meiram@kbtu.kz>
2026-03-27 11:54:44 +0500

    lab 5

--- ejudge/working_space.py
+++ ejudge/working_space.py
@@ -1,4 +1,5 @@
-a, b = map(int, input().split())
-sum = a * b
+n = int(input())
+a = list(map(str, input().split()))
+sum = list(lambda x_i : x_i[0] % 2 == 0, enumerate(a))
+even = [x for i, x in sum]
 print(sum)
-
